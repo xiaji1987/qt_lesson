1c4973f1d0c4c234fea634e0b291215752b39cde
xiaji1987 <3094889344@qq.com>
2019-09-04 23:08:19 +0800

    vue中的element的用法

---
+++ vue/vue-element/src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-content">
+    <!-- 头部header -->
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from '@/common/Header'
+export default {
+  name: 'mainContent',
+  data () {
+    return {}
+  },
+  components: {
+    Header
+  }
+}
+</script>
+
+<style scoped>
+</style>
