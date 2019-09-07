3f013c48e6a1e0bc18ae0162d1d9571965fb4d82
xiaji1987 <3094889344@qq.com>
2019-09-07 22:22:21 +0800

    vue做抖音网红时钟

---
+++ vue/vue-clock/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <Second :second="second"/>
+    <Minute/>
+    <Hour/>
+    <Apm/>
+    <Week/>
+    <Day/>
+    <Month/>
+  </div>
+</template>
+
+<script>
+import Second from '@/components/Second.vue'
+import Minute from '@/components/Minute.vue'
+import Hour from '@/components/Hour.vue'
+import Apm from '@/components/Apm.vue'
+import Week from '@/components/Week.vue'
+import Day from '@/components/Day.vue'
+import Month from '@/components/Month.vue'
+export default {
+  name: 'App',
+  components: {
+    Second: Second,
+    Minute: Minute,
+    Hour: Hour,
+    Apm: Apm,
+    Week: Week,
+    Day: Day,
+    Month: Month,
+  },
+  data () {
+    return {
+      second: ''
+    }
+  },
+  methods: {
+    start () {
+      setInterval (() => {
+        let date = new Date();
+        this.second = date.getSeconds();
+      }, 1000)
+    }
+  },
+  created () {
+    this.start();
+  }
+}
+</script>
+
+<style>
+
+</style>
