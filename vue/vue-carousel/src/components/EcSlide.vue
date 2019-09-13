bf77f46d945f7a02e79d0ecf0299b07cf733f1c0
xiaji1987 <3094889344@qq.com>
2019-09-13 23:00:14 +0800

    vue做幻灯片

---
+++ vue/vue-carousel/src/components/EcSlide.vue
@@ -0,0 +1,42 @@
+<template>
+  <div  class="switch-img-box" id="ec-slide-box" :style="{width: swidth + 'px'}"></div>
+</template>
+
+<script>
+export default {
+  name: 'EcSlide',
+  data () {
+    return {
+      timer: null, // 定时器 组建自己打理， */
+    }
+  },
+  // 外界的传参， 更好定制
+  props: {
+    time: {
+      type: Number,
+      default: 4000
+    },
+    autoplay: {
+      type: Boolean,
+      default: true
+    },
+    swidth: {
+      type: Number
+    }
+  },
+  methods: {
+    
+  },
+  computed: {
+    
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+// stylus stylus-loader
+.switch-img-box
+  width 100%
+  height 100%
+  position relative
+</style>
