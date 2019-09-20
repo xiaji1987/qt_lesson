d015a8accd2007e2d493619fa108c1170d1a94a6
xiaji1987 <3094889344@qq.com>
2019-09-20 23:30:14 +0800

    'vue做一个网易云音乐'

---
+++ vue/vue-cloud-music/src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+import der_lazy_img from '../static/img/loading.gif'
+
+Vue.use(VueLazyload,{
+  loading: der_lazy_img
+})
+fastclick.attach(document.body)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
