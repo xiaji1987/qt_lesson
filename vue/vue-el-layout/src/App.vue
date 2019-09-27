15d007782efc8a19e3098a8da86d1604108cbce3
xiaji1987 <3094889344@qq.com>
2019-09-27 23:03:27 +0800

    'vue中的render(h)解析'

---
+++ vue/vue-el-layout/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <el-button :plain="true" @click="openVn">VNode</el-button> -->
+    <!-- <img src="./assets/logo.png" /> -->
+    <!-- <router-view /> -->
+    <my-elrow tag="div" :jiange="20">
+      <my-elcol :span="4">col-1</my-elcol>
+      <my-elcol :span="20" :offset="4">col-2</my-elcol>
+    </my-elrow>
+  </div>
+</template>
+
+<script>
+import ElRow from './components/El-Row'
+import ElCol from './components/El-Col'
+export default {
+  name: "App",
+  components: {
+    'my-elrow': ElRow,
+    'my-elcol': ElCol
+  }
+};
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ vue/vue-el-layout/src/components/El-Row.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: {
+    tag: {
+      type: String, 
+      default: 'div'
+    },
+    jiange: {
+      type: Number
+    }
+  },
+  render(h) {
+    return h(
+      this.tag, 
+      {
+        class: ['el-row']
+      },
+      this.$slots.default
+    )
+  }
+}
+</script>
+
+<style scoped>
+.el-row{
+  width: 100%;
+  display: flex;
+}
+</style>
