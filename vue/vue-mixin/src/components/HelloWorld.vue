c9fc4d62b1954ee98555bf3f08e22dbc2178599b
xiaji1987 <3094889344@qq.com>
2019-10-11 18:59:58 +0800

    'vue中mixin的作用'

---
+++ vue/vue-mixin/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Mydialog />
+    <Myloading />
+  </div>
+</template>
+
+<script>
+import Mydialog from './my-dialog'
+import Myloading from './my-loading'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      
+    }
+  },
+  components: {
+    Mydialog,
+    Myloading
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
