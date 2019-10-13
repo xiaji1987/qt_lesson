ee7ab59abfb5acbc52133eb8aa94aef753048665
xiaji1987 <3094889344@qq.com>
2019-10-13 22:42:30 +0800

    'vue实验楼20+的页面'

---
+++ vue/vue-shiyanlou/README.md
@@ -0,0 +1,26 @@
+- 项目的构架， 目录
+  1. api 前后端分离  
+    vue的前端
+    ajax 连接后端 axios
+    /api/v2/products/:1
+  2. pages compents
+  3. vuex store 设置？
+  4. utils js 公共方法
+  5. css/stylus stylus用的多牛逼
+  20+页面的架构
+
+- 路由
+  懒加载
+
+- page 组件化
+
+- 页面由组件构成
+  页面级组件 /pages
+  可复用的组件 /componments
+    - 全站常用的公共组件， 跟业务无关 iview /compoments/common/
+  容器组件
+    PC 页面量 切页面的工作交给组件化 html section概念做的
+
+- App.vue 根组件， reset 工作  通用样式， 
+
+- 界面加状态
---
+++ vue/vue-shiyanlou/src/pages/home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="home=page">
+    <NavigationBar />
+    <MainNavgation />
+    <LouPlus />
+    <Bountique />
+    <RecentlyRecommendCourse />
+    <BaseCourse />
+    <BackendCourse />
+    <CloudComputeBigDataCourse />
+    <TrainingCourse />
+    <LearningCourse />
+    <SiteDescription />
+  </div>
+</template>
+
+<script>
+import NavigationBar from '@/components/home_page/navigation_bar'
+import MainNavgation from '@/components/home_page/main_navgation'
+import LouPlus from '@/components/home_page/lou_plus'
+import Bountique from '@/components/home_page/bountique'
+import RecentlyRecommendCourse from '@/components/home_page/recently_recommend_course'
+import BaseCourse from '@/components/home_page/base_course'
+import BackendCourse from '@/components/home_page/backend_course'
+import CloudComputeBigDataCourse from '@/components/home_page/cloud_compute_big_data_course'
+import TrainingCourse from '@/components/home_page/training_course'
+import LearningCourse from '@/components/home_page/learning_course'
+import SiteDescription from '@/components/home_page/site_description'
+export default {
+  components: {
+    NavigationBar,
+    MainNavgation,
+    LouPlus,
+    Bountique,
+    RecentlyRecommendCourse,
+    BaseCourse,
+    BackendCourse,
+    CloudComputeBigDataCourse,
+    TrainingCourse,
+    LearningCourse,
+    SiteDescription
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
