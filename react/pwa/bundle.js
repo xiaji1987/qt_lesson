26ebd19b3e94e3013923101a9daf89db0fba0886
xiaji1987 <3094889344@qq.com>
2019-12-28 17:00:53 +0800

    pwa的使用

---
+++ react/pwa/bundle.js
@@ -0,0 +1,9 @@
+document.getElementById('app').innerText = '46号图片'
+
+setTimeout(() => {
+  fetch('http://rap2api.taobao.org/app/mock/240109/redux/todolist').then(res => {
+    return res.json()
+  }).then(res => {
+    console.log(res)
+  })
+}, 3000)
